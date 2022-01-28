f5aded66c510c0741667e9ddc07e5e3a6b3e9647
ejmejm <ninjaedan@aol.com>
2022-01-28 04:38:30 -0700

    Added preliminary sweep configs and support for boolean params in sweeps

--- sweep_agent.py
+++ sweep_agent.py
@@ -1,14 +1,16 @@
+import argparse
 import sys
 
 import wandb
 
 sys.path.append('..')
 
-from src.experiments.argument_handling import make_and_parse_args
+from src.experiments.argument_handling import make_and_parse_args, format_args
 from src.experiments.training import train_loop
 
 
 if __name__ == '__main__':
     args = make_and_parse_args()
     wandb.init(config=args)
+    args = format_args(argparse.Namespace(**wandb.config))
     train_loop(args)
